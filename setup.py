c145f0670f75edc1008285400db5b2d1fa186402
Xiaoye Xu <xiaoyex@dunnhumby.com>
2019-08-23 12:15:05 +0800

    updated

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="myrest",
     version="0.1.0",
+    author="Xiaoye Xu",
+    author_email="xiaoye.xu@outlook.com",
+    description="A RESTful wrapper for django project",
+    long_description=long_description,
+    url="https://github.com/xiaoyexu/myrestengine",
     packages=find_packages('src'),
     package_dir={'': 'src'},
     package_data={
@@ -10,5 +18,10 @@ setup(
     },
     install_requires=[
         'django>=1.9.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Framework :: Django :: 1.9",
+        "License :: OSI Approved :: MIT License"
     ]
 )
--- src/myrest/__init__.py
+++ src/myrest/__init__.py
@@ -1,3 +1,3 @@
 VERSION = (0, 1, 0)
-
+name = "myrest"
 __all__ = ['myparser', 'myrestengine']
